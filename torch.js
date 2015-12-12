f291b9cecf0d084f7506206850271f635cdf79d4
Talor Anderson <ta.anders17@gmail.com>
2015-12-11 19:20:42 -0800

    Basic site done, needs content

--- torch.js
+++ torch.js
@@ -1,11 +1,18 @@
+//affix navbar
 $('#nav').affix({
       offset: {
         top: $('header').height()
       }
 });
 
+//affix sidebar
 $('#sidebar').affix({
       offset: {
         top: 280
       }
 });
+
+
+//center navbar
+var width = $('.justified').width();
+$('.justified').css('margin-left', '-' + (width / 2)+'px');
